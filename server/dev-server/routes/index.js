302a68ebaaad7457bea7f9a2da3bde4a135c634d
Marco Augusto <up201800388@edu.fc.up.pt>
2021-10-27 21:13:40 +0100

    added reply to capabilities

--- server/dev-server/routes/index.js
+++ server/dev-server/routes/index.js
@@ -9,6 +9,31 @@ var data = []
 
 var router = express.Router();
 
+router.get('/capabilities', function(req, res, next) {
+    let evalRes = new EvaluationReport()
+    let obj = {
+        "capabilities": [{
+            "id": "XPath-evaluator",
+            "features": [{
+                "name": "language",
+                "value": "XPath"
+            }, {
+                "name": "version",
+                "value": ".01"
+            }, {
+                "name": "engine",
+                "value": "https://www.npmjs.com/package/xpath"
+            }]
+        }]
+
+    }
+    console.log(typeof obj)
+    evalRes.setReply(obj)
+    res.send(evalRes)
+
+});
+
+
 
 router.get('/', function(req, res, next) {
     res.send(getAvailableExercise())
@@ -18,24 +43,17 @@ router.post('/eval', function(req, res, next) {
     if (evalReq.setRequest(req.body)) {
         if ('program' in evalReq.request) {
             try {
-
                 ProgrammingExercise.deserialize(ProgrammingExercise.serializedPath(), `${evalReq.request.learningObject}.zip`).then(programmingExercise => {
                     let obj = evaluator.XPATH(programmingExercise, evalReq.request.program)
-
                     if (!data.includes(programmingExercise.id)) {
                         data.push(programmingExercise.id)
                         programmingExercise.serialize(path.join(__dirname, "../../public/zip")).then(test => {
                             if (test) {
                                 console.log(`The exercise ${programmingExercise.id} was insert in cache`)
                             }
-
                         })
-
                     }
                     res.json(obj)
-
-
-
                 })
             } catch (error) {
                 console.log(error)
