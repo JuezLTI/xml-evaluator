3f7558a416d7e9c3329f3dc0b17178c923fbbdc6
Marco <up201800388@up.pt>
2022-02-11 10:09:52 -0300

    Updating xml to support changes in the feedback-manager

--- server/dev-server/routes/index.js
+++ server/dev-server/routes/index.js
@@ -13,32 +13,32 @@ var data = [];
 
 var router = express.Router();
 
-router.get("/capabilities", function(req, res, next) {
+router.get("/capabilities", function (req, res, next) {
     let evalRes = new EvaluationReport();
     let obj = {
         capabilities: [{
             id: "XPath-evaluator",
             features: [{
-                    name: "language",
-                    value: "XPath",
-                },
-                {
-                    name: "version",
-                    value: ".01",
-                },
-                {
-                    name: "engine",
-                    value: "https://www.npmjs.com/package/xpath",
-                },
+                name: "language",
+                value: "XPath",
+            },
+            {
+                name: "version",
+                value: ".01",
+            },
+            {
+                name: "engine",
+                value: "https://www.npmjs.com/package/xpath",
+            },
             ],
-        }, ],
+        },],
     };
 
     evalRes.setReply(obj);
     res.send(evalRes);
 });
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     let text = fs.readFileSync(
         path.join(__dirname, "../../public/doc/intro.txt"), { encoding: "utf8", flag: "r" }
     );
@@ -53,7 +53,7 @@ router.get("/", function(req, res) {
     }
 });
 
-router.post("/eval", function(req, res, next) {
+router.post("/eval", function (req, res, next) {
 
     loadSchemaPEARL().then(() => {
         console.log(req.body)
@@ -65,19 +65,19 @@ router.post("/eval", function(req, res, next) {
                     let exerciseObj = new ProgrammingExercise();
                     if (data.includes(evalReq.request.learningObject)) {
                         ProgrammingExercise.deserialize(path.join(__dirname, "../../public/zip"), `${evalReq.request.learningObject}.zip`).
-                        then((programmingExercise) => {
-                            evaluate(programmingExercise, evalReq, req, res, next)
-                        }).catch((error) => {
-                            console.log("error " + error);
-                            res.statusCode(500).send("The learning object request is already in cache but was not possible to read")
-                        })
+                            then((programmingExercise) => {
+                                evaluate(programmingExercise, evalReq, req, res, next)
+                            }).catch((error) => {
+                                console.log("error " + error);
+                                res.statusCode(500).send("The learning object request is already in cache but was not possible to read")
+                            })
                     } else {
                         loadSchemaYAPEXIL().then(() => {
                             ProgrammingExercise
-                                .loadRemoteExercise(evalReq.request.learningObject,{
-                                    'BASE_URL':process.env.BASE_URL,
-                                    'EMAIL':process.env.EMAIL,
-                                    'PASSWORD':process.env.PASSWORD,
+                                .loadRemoteExercise(evalReq.request.learningObject, {
+                                    'BASE_URL': process.env.BASE_URL,
+                                    'EMAIL': process.env.EMAIL,
+                                    'PASSWORD': process.env.PASSWORD,
                                 })
                                 .then((programmingExercise) => {
 
@@ -120,38 +120,41 @@ router.post("/eval", function(req, res, next) {
 
 function evaluate(programmingExercise, evalReq, req, res, next) {
     evaluator.XPATH(programmingExercise, evalReq).then((obj) => {
-       console.log("Resposta ->" + JSON.stringify(obj))
+        console.log("Resposta ->" + JSON.stringify(obj))
+        //user id. how  does this property will work ?
+        obj.reply.report.user_id = 1
+        obj.reply.report.number_of_tests = programmingExercise.getTests().length
         req.xpath_eval_result = JSON.stringify(obj);
-        req.number_of_tests = programmingExercise.getTests().length
+
         /*     if (obj.reply.report.compilationErrors.length > 0) {
                 res.send("Incorrect Answer\n").status(200);
             } else {
                 res.send("Correct Answer\n").status(200);
 
             }*/
-            
+
         next();
     });
 }
 
 
-router.post("/eval", function(req, res, next) {
+router.post("/eval", function (req, res, next) {
 
     request({
-            method: "POST",
-            url: process.env.FEEDBACK_MANAGER_URL,
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ PEARL: req.xpath_eval_result, additional: { numberOfTests: req.number_of_tests } })
+        method: "POST",
+        url: process.env.FEEDBACK_MANAGER_URL,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
         },
-        function(error, response) {
-            
-            if (error!=null){
+        body: JSON.stringify({ PEARL: req.xpath_eval_result })
+    },
+        function (error, response) {
+
+            if (error != null) {
                 console.log(error)
                 res.json(error);
-            } 
+            }
             else res.json(response.body);
         }
     );
